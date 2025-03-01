3d253906bc0ec7eeb1b4357d007df0ce16432783
Winner <winner@Winners-MBP.lan>
2025-03-01 14:00:55 +0200

    polish sidebar animations and welcome page

--- front-end/src/pages/Welcome/welcome.js
+++ front-end/src/pages/Welcome/welcome.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -6,54 +6,82 @@ import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Image from 'react-bootstrap/Image';
-
 import discoveryImage from '../../assets/discoverImage.jpeg';
-
 import styles from "./welcome.module.css";
 
 const Welcome = () => {
-	return (
-		<div className={styles.main} >
-			{/* Navigation Bar */}
-			<Navbar variant="dark" className={styles.navBar} >
-				<Navbar.Brand className={styles.name}>Chesster</Navbar.Brand>
-				<Nav className="mr-auto">
-					<Nav.Link href="home">Home</Nav.Link>
-					<Nav.Link href="news">News</Nav.Link>
-					<Nav.Link href="home">Play</Nav.Link>
-					<Nav.Link href="puzzles">Puzzles</Nav.Link>
-				</Nav>
-				<Nav className="ms-auto">
-					<Button variant="outline-light" href='login' className={styles.login} >Log in</Button>
-					<Button href='signup' variant="danger" className={styles.register}>Sign up</Button>
-				</Nav>
-			</Navbar>
-
-			{/* Main Content */}
-			<Container fluid className={`${styles.mainContainer} d-flex align-items-center justify-content-around`} >
-				<Row>
-					<Col md={6} className="text-left">
-						<h1 className={styles.discoverHeader} >
-							Discover chess tactics & strategies for victory!
-						</h1>
-						<p style={{ fontSize: '1.1em', marginBottom: '30px' }}>Start your chess journey now.</p>
-						<Button href='login' variant="danger" className={styles.start} >
-							Start playing
-						</Button>
-					</Col>
-					<Col md={6} className="text-right">
-						<Image
-							src={discoveryImage}
-							alt="Chess Pieces"
-							style={{ width: '80%', borderRadius: '15px' }}
-							fluid
-						/>
-						<p style={{ fontSize: '0.8em', marginTop: '10px', textAlign: 'center' }}>Chess, Strategy, Pieces</p>
-					</Col>
-				</Row>
-			</Container>
-		</div>
-	);
+  // Animation effect when component mounts
+  useEffect(() => {
+    const animateElements = () => {
+      document.querySelector(`.${styles.navBar}`).classList.add(styles.fadeInDown);
+      document.querySelector(`.${styles.contentLeft}`).classList.add(styles.slideInLeft);
+      document.querySelector(`.${styles.contentRight}`).classList.add(styles.slideInRight);
+    };
+    
+    // Small delay for animation to start after page load
+    setTimeout(animateElements, 300);
+  }, []);
+  
+  return (
+    <div className={styles.main}>
+      {/* Navigation Bar */}
+      <Navbar variant="dark" className={styles.navBar}>
+        <Navbar.Brand className={styles.name}>
+          <span className={styles.logoText}>C</span>hesster
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="home" className={styles.navLink}>Home</Nav.Link>
+          <Nav.Link href="news" className={styles.navLink}>News</Nav.Link>
+          <Nav.Link href="home" className={styles.navLink}>Play</Nav.Link>
+          <Nav.Link href="puzzles" className={styles.navLink}>Puzzles</Nav.Link>
+        </Nav>
+        <Nav className="ms-auto">
+          <Button variant="outline-light" href='login' className={styles.login}>
+            Log in
+          </Button>
+          <Button href='signup' variant="primary" className={styles.register}>
+            Sign up
+          </Button>
+        </Nav>
+      </Navbar>
+      
+      {/* Main Content */}
+      <Container fluid className={`${styles.mainContainer} d-flex align-items-center justify-content-around`}>
+        <Row className="w-100">
+          <Col md={6} className={`text-left ${styles.contentLeft}`}>
+            <h1 className={styles.discoverHeader}>
+              Discover chess tactics & strategies for 
+              <span className={styles.victoryText}> victory!</span>
+            </h1>
+            <p className={styles.subtitle}>Start your chess journey now.</p>
+            <Button href='login' variant="primary" className={styles.start}>
+              <span className={styles.btnContent}>
+                Start playing
+                <span className={styles.chessPiece}>♞</span>
+              </span>
+            </Button>
+          </Col>
+          <Col md={6} className={`text-right ${styles.contentRight}`}>
+            <div className={styles.imageContainer}>
+              <Image
+                src={discoveryImage}
+                alt="Chess Pieces"
+                className={styles.mainImage}
+                fluid
+              />
+              <div className={styles.imageShadow}></div>
+            </div>
+            <p className={styles.imageCaption}>Chess, Strategy, Pieces</p>
+          </Col>
+        </Row>
+      </Container>
+      
+      {/* Animated Background Elements */}
+      <div className={styles.chessPiece1}>♜</div>
+      <div className={styles.chessPiece2}>♝</div>
+      <div className={styles.chessPiece3}>♚</div>
+    </div>
+  );
 };
 
 export default Welcome;
